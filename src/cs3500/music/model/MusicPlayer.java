90db984af234480dee5bf2a920572a4ae4cb4486
tbette1 <bettencourt.t@husky.neu.edu>
2016-03-16 11:10:20 -0400

    Update MusicPlayer.java

--- src/cs3500/music/model/MusicPlayer.java
+++ src/cs3500/music/model/MusicPlayer.java
@@ -1,4 +1,52 @@
 package cs3500.music.model;
 
 public class MusicPlayer {
+    Song track;
+    static int beat;
+
+    /**
+     * Constructs a new MusicPlayer object.
+     */
+    public MusicPlayer() {
+        track = null;
+        beat = -1;
+    }
+
+    /**
+     * Returns true if the playing song is over, returns
+     * false otherwise.
+     * throws IllegalStateException if called while the song is not playing.
+     */
+    public boolean isSongOver() throws IllegalStateException {
+        if (beat == -1) {
+            throw new IllegalStateException("Song has not started playing");
+        }
+        else {
+            return beat >= track.getLength();
+        }
+    }
+
+    /**
+     * Adds the given song to this MusicPlayer to start at the
+     * given beat. If another song exists at that beat, the song
+     * to be added is layered over it.
+     */
+    public void add(Song s, int beat) {
+
+    }
+
+    /**
+     * Starts play for this MusicPlayer's song.
+     */
+    public void play() {
+
+    }
+
+
+    /**
+     * Moves this MusicPlayers metrinomic beat one beat forward.
+     */
+    public void tick() {
+        beat ++;
+    }
 }
