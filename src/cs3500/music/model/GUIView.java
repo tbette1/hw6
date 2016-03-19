96bbd0800876afd5a63ee4d45b5b3d46bd16246f
tbette1 <bettencourt.t@husky.neu.edu>
2016-03-18 23:09:16 -0400

    Added files via upload

---
+++ src/cs3500/music/model/GUIView.java
@@ -0,0 +1,7 @@
+package cs3500.music.model;
+
+/**
+ * Created by torybettencourt on 3/18/16.
+ */
+public interface GUIView extends View {
+}
---
+++ src/cs3500/music/model/GraphicView.java
@@ -0,0 +1,7 @@
+package cs3500.music.model;
+
+/**
+ * Created by torybettencourt on 3/18/16.
+ */
+public class GraphicView implements GUIView {
+}
