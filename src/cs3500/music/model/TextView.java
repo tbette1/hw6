f6aea45d81dc5dfc7497fa4936c1ae0bd4b32018
tbette1 <bettencourt.t@husky.neu.edu>
2016-03-20 01:05:42 -0400

    Update TextView.java

--- src/cs3500/music/model/TextView.java
+++ src/cs3500/music/model/TextView.java
@@ -4,4 +4,9 @@ package cs3500.music.model;
  * Created by torybettencourt on 3/18/16.
  */
 public class TextView implements View {
+  
+  
+  public String output(MVConnection mvconnect) {
+    
+  }
 }
